23c3eab4716e26af84ed8b7cc6eb2813c5fb90a1
hytcchenjie <1635232389@qq.com>
2014-10-14 17:18:46 +0800

    gengxin

--- starWeibo/starWeibo/adminManage.aspx.cs
+++ starWeibo/starWeibo/adminManage.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using starweibo.BLL;
 
 namespace starWeibo
 {
@@ -11,7 +12,7 @@ namespace starWeibo
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }

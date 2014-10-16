b54936c15cde25fab0d9dd72560d02ba9c0d73be
hytcshenyazhou <641077955@qq.com>
2014-10-16 17:20:23 +0800

    换肤
    换肤完成

--- starWeibo/starWeibo/starweiboM.Master.cs
+++ starWeibo/starWeibo/starweiboM.Master.cs
@@ -9,9 +9,11 @@ namespace starWeibo
 {
     public partial class starweiboM : System.Web.UI.MasterPage
     {
+        public int unreadCount = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            starweibo.BLL.messageInfo wrwe = new starweibo.BLL.messageInfo();
+            unreadCount = wrwe.GetRecordCount("userId=1 and msgState='noread'");
         }
     }
 }

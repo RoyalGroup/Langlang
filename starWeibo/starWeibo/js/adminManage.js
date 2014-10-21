0b110b9cad9e7719d68461c26134ac9564cd5217
hytcchenjie <1635232389@qq.com>
2014-10-21 17:25:57 +0800

    权限操作完善

--- starWeibo/starWeibo/js/adminManage.js
+++ starWeibo/starWeibo/js/adminManage.js
@@ -47,21 +47,21 @@
     });
 
     $(".Delete").click(function () {
-        var messageID = $(this).prev().prev().attr("msgID");
+        var messageID = $(this).prev().attr("msgID");
         var blogID = $(this).attr("blogID");
         $(this).parent().parent().remove();
-        //$.ajax({
-        //    type: "POST",   //访问WebService使用Post方式请求
-        //    contentType: "application/json", //WebService 会返回Json类型
-        //    url: "webservice/wsadmin.asmx/Delete", //调用WebService的地址和方法名称组合 ---- WsURL/方法名
-        //    data: "{blogId:" + blogID + "}",
-        //    dataType: 'json',
-        //    success: function (result) {     //回调函数，result，返回值
-        //        if (result.d == true) {
-        //            alert("删除成功！");
-        //        }
-        //    }
-        //});
+        $.ajax({
+            type: "POST",   //访问WebService使用Post方式请求
+            contentType: "application/json", //WebService 会返回Json类型
+            url: "webservice/wsadmin.asmx/Delete", //调用WebService的地址和方法名称组合 ---- WsURL/方法名
+            data: "{blogId:" + blogID + ",messageId:" + messageID + "}",
+            dataType: 'json',
+            success: function (result) {     //回调函数，result，返回值
+                if (result.d == true) {
+                    //alert("删除成功！");
+                }
+            }
+        });
     });
 
     $(".jinyan").click(function () {
@@ -76,7 +76,7 @@
             dataType: 'json',
             success: function (result) {     //回调函数，result，返回值
                 if (result.d == true) {
-                    //alert("禁言成功！");
+                    alert("禁言成功！");
                 }
             }
         });
@@ -102,8 +102,9 @@
 
     //用户权限管理
     $(".select").click(function () {
-        var userID = $(this).parent().parent().attr("id");
-        var content = $(this).attr("content");
+        var con = $(this).parent().parent().attr("id");
+        var userID = con.substring(1);
+        var id = $(this).attr("id");
         //var fuzhi = $(this).attr("fuzhi");
         //if (fuzhi == "0") {
         //    $(this).attr("fuzhi", "1");
@@ -116,7 +117,7 @@
             type: "POST",   //访问WebService使用Post方式请求
             contentType: "application/json", //WebService 会返回Json类型
             url: "webservice/wsadmin.asmx/quanxian", //调用WebService的地址和方法名称组合 ---- WsURL/方法名
-            data: "{userId:" + userID + ",type:'" + content + "',fuzhi:'" + fuzhi + "'}",
+            data: "{userId:" + userID + ",type:'" + id + "',fuzhi:'" + fuzhi + "'}",
             dataType: 'json',
             success: function (result) {     //回调函数，result，返回值
                 if (result.d == true) {
@@ -124,5 +125,144 @@
                 }
             }
         });
+    });
+
+    $(".quan").click(function () {
+        //var userID = $(".selectOne:checked").parent().parent().attr("id");
+        $(".selectOne:checked").parent().nextAll().find("input").prop("checked", true);
+        
+        var a = new Array();
+
+        var checkBoxes = document.getElementsByName("selectO");
+        for (var i = 1; i <=checkBoxes.length+1; i++) {
+            if ($("#s" + i).find(".selectOne").prop("checked") == true)
+            {
+                a[i] = i;
+            }
+            
+        };
+        
+        for (var i = 1; i <= checkBoxes.length+1; i++) {
+            if (a[i] != null);
+            {
+                $.ajax({
+                    type: "POST",   //访问WebService使用Post方式请求
+                    contentType: "application/json", //WebService 会返回Json类型
+                    url: "webservice/wsadmin.asmx/quan", //调用WebService的地址和方法名称组合 ---- WsURL/方法名
+                    data: "{userId:" + a[i] + "}",
+                    dataType: 'json',
+                    success: function (result) {     //回调函数，result，返回值
+                        if (result.d == true) {
+                            //alert("操作成功！");
+                        }
+                    }
+                });
+            }
+        };
+        
+    });
+    $(".kquan").click(function () {
+        //var userID = $(".selectOne:checked").parent().parent().attr("id");
+        $(".selectOne:checked").parent().nextAll().find("input").prop("checked", false);
+        var a = new Array();
+
+        var checkBoxes = document.getElementsByName("selectO");
+        for (var i = 1; i <= checkBoxes.length + 1; i++) {
+            if ($("#s" + i).find(".selectOne").prop("checked") == true) {
+                a[i] = i;
+            }
+
+        };
+
+        for (var i = 1; i <= checkBoxes.length + 1; i++) {
+            if (a[i] != null);
+            {
+                $.ajax({
+                    type: "POST",   //访问WebService使用Post方式请求
+                    contentType: "application/json", //WebService 会返回Json类型
+                    url: "webservice/wsadmin.asmx/kquan", //调用WebService的地址和方法名称组合 ---- WsURL/方法名
+                    data: "{userId:" + a[i] + "}",
+                    dataType: 'json',
+                    success: function (result) {     //回调函数，result，返回值
+                        if (result.d == true) {
+                            //alert("操作成功！");
+                        }
+                    }
+                });
+            }
+        };
+    });
+
+    //quanxian
+    $(".fa").click(function () {
+        var type = $(this).attr("id");
+        $(".selectOne:checked").parent().nextAll().find('input[id="' + type + '"]').prop("checked", true);
+
+        var a = new Array();
+
+        var checkBoxes = document.getElementsByName("selectO");
+        for (var i = 1; i <= checkBoxes.length + 1; i++) {
+            if ($("#s" + i).find(".selectOne").prop("checked") == true) {
+                a[i] = i;
+            }
+
+        };
+
+        for (var i = 1; i <= checkBoxes.length + 1; i++) {
+            if (a[i] != null);
+            {
+                $.ajax({
+                    type: "POST",   //访问WebService使用Post方式请求
+                    contentType: "application/json", //WebService 会返回Json类型
+                    url: "webservice/wsadmin.asmx/xuanze", //调用WebService的地址和方法名称组合 ---- WsURL/方法名
+                    data: "{userId:" + a[i] + ",type:'"+ type +"'}",
+                    dataType: 'json',
+                    success: function (result) {     //回调函数，result，返回值
+                        if (result.d == true) {
+                            //alert("操作成功！");
+                        }
+                    }
+                });
+            }
+        };
+    });
+
+    $(".jie").click(function () {
+        var con = $(this).attr("id");
+        var type = con.substring(1);
+        $(".selectOne:checked").parent().nextAll().find('input[id="' + type + '"]').prop("checked", false);
+        var a = new Array();
+
+        var checkBoxes = document.getElementsByName("selectO");
+        for (var i = 1; i <= checkBoxes.length + 1; i++) {
+            if ($("#s" + i).find(".selectOne").prop("checked") == true) {
+                a[i] = i;
+            }
+
+        };
+
+        for (var i = 1; i <= checkBoxes.length + 1; i++) {
+            if (a[i] != null);
+            {
+                $.ajax({
+                    type: "POST",   //访问WebService使用Post方式请求
+                    contentType: "application/json", //WebService 会返回Json类型
+                    url: "webservice/wsadmin.asmx/xuanzek", //调用WebService的地址和方法名称组合 ---- WsURL/方法名
+                    data: "{userId:" + a[i] + ",type:'" + type + "'}",
+                    dataType: 'json',
+                    success: function (result) {     //回调函数，result，返回值
+                        if (result.d == true) {
+                            alert("操作成功！");
+                        }
+                    }
+                });
+            }
+        };
+    });
+
+    //搜索
+    $(".button").click(function () {
+        $(".UserManagesContentS").show();
+        $(".UserManagesContent").hide();
     });
 });
--- starWeibo/starWeibo/webservice/wsadmin.asmx.cs
+++ starWeibo/starWeibo/webservice/wsadmin.asmx.cs
@@ -24,7 +24,6 @@ namespace starWeibo.webservice
             return "Hello World";
         }
 
-        private starweibo.BLL.messageInfo Msg = new starweibo.BLL.messageInfo();
         [WebMethod]
         //忽略举报信息
         public bool hulueReport(int messageId)
@@ -33,29 +32,29 @@ namespace starWeibo.webservice
             starweibo.BLL.messageInfo bllmsg = new starweibo.BLL.messageInfo();
             onemsg = bllmsg.GetModel(messageId);
             onemsg.msgState = "read";
-            return Msg.Update(onemsg);
+            return bllmsg.Update(onemsg);
         }
 
-        //private starweibo.BLL.blogInfo blog = new starweibo.BLL.blogInfo();
-        //[WebMethod]
-        ////被举报微博删除
-        //public bool Delete(int messageId, int blogId)
-        //{
-        //    starweibo.Model.messageInfo onemsg = new messageInfo();
-        //    starweibo.BLL.messageInfo bllmsg = new starweibo.BLL.messageInfo();
-        //    onemsg = bllmsg.GetModel(messageId);
-        //    onemsg.msgState = "read";
-        //    Msg.Update(onemsg);
+        
+        [WebMethod]
+        //被举报微博删除
+        public bool Delete(int messageId, int blogId)
+        {
+            starweibo.Model.messageInfo onemsg = new messageInfo();
+            starweibo.BLL.messageInfo bllmsg = new starweibo.BLL.messageInfo();
+            onemsg = bllmsg.GetModel(messageId);
+            onemsg.msgState = "read";
+            bllmsg.Update(onemsg);
 
-        //    starweibo.Model.blogInfo oneblog = new blogInfo();
-        //    starweibo.BLL.blogInfo bllmsgP = new starweibo.BLL.blogInfo();
-        //    oneblog = bllmsgP.GetModel(blogId);
-        //    oneblog.blogState = "delete";
+            starweibo.Model.blogInfo oneblog = new blogInfo();
+            starweibo.BLL.blogInfo bllmsgP = new starweibo.BLL.blogInfo();
+            oneblog = bllmsgP.GetModel(blogId);
+            oneblog.blogState = "1";
 
-        //    return blog.Update(oneblog);
-        //}
+            return bllmsgP.Update(oneblog);
+        }
 
-        private starweibo.BLL.powerInfo pow = new starweibo.BLL.powerInfo();
+        
         [WebMethod]
         //被举报用户禁言
         public bool jinyan(int messageId, int userId)
@@ -64,14 +63,14 @@ namespace starWeibo.webservice
             starweibo.BLL.messageInfo bllmsg = new starweibo.BLL.messageInfo();
             onemsg = bllmsg.GetModel(messageId);
             onemsg.msgState = "read";
-            Msg.Update(onemsg);
+            bllmsg.Update(onemsg);
 
             starweibo.Model.powerInfo onemsgP = new powerInfo();
             starweibo.BLL.powerInfo bllmsgP = new starweibo.BLL.powerInfo();
             onemsgP = bllmsgP.GetModel(userId);
             onemsgP.fasong = "0";
 
-            return pow.Update(onemsgP);
+            return bllmsgP.Update(onemsgP);
         }
 
         //private starweibo.BLL.chatInfo SysMsg = new starweibo.BLL.chatInfo();
@@ -121,7 +120,121 @@ namespace starWeibo.webservice
                     usp.fasong = fuzhi;
                     break;
             }
-            return pow.Update(usp);
+            return bllusp.Update(usp);
+        }
+
+        //用户权限全禁
+        [WebMethod]
+        //用户权限全禁
+        public bool quan(int userId)
+        {
+            starweibo.Model.powerInfo usp = new powerInfo();
+            starweibo.BLL.powerInfo bllusp = new starweibo.BLL.powerInfo();
+            usp = bllusp.GetModel(userId);
+
+            usp.zan = "0";
+            usp.pinglun = "0";
+            usp.at = "0";
+            usp.guanzhu = "0";
+            usp.jubao = "0";
+            usp.zhuanfa = "0";
+            usp.shoucang = "0";
+            usp.fasong = "0";
+
+            return bllusp.Update(usp);
+        }
+
+        [WebMethod]
+        //用户权限全解
+        public bool kquan(int userId)
+        {
+            starweibo.Model.powerInfo usp = new powerInfo();
+            starweibo.BLL.powerInfo bllusp = new starweibo.BLL.powerInfo();
+            usp = bllusp.GetModel(userId);
+
+            usp.zan = "1";
+            usp.pinglun = "1";
+            usp.at = "1";
+            usp.guanzhu = "1";
+            usp.jubao = "1";
+            usp.zhuanfa = "1";
+            usp.shoucang = "1";
+            usp.fasong = "1";
+
+            return bllusp.Update(usp);
+        }
+
+        [WebMethod]
+        //用户权选择更改
+        public bool xuanze(int userId, string type)
+        {
+            starweibo.Model.powerInfo usp = new powerInfo();
+            starweibo.BLL.powerInfo bllusp = new starweibo.BLL.powerInfo();
+            usp = bllusp.GetModel(userId);
+            switch (type)
+            {
+                case "zan":
+                    usp.zan = "0";
+                    break;
+                case "pinglun":
+                    usp.pinglun = "0";
+                    break;
+                case "at":
+                    usp.at = "0";
+                    break;
+                case "guanzhu":
+                    usp.guanzhu = "0";
+                    break;
+                case "jubao":
+                    usp.jubao = "0";
+                    break;
+                case "zhuanfa":
+                    usp.zhuanfa = "0";
+                    break;
+                case "shoucang":
+                    usp.shoucang = "0";
+                    break;
+                case "fasong":
+                    usp.fasong = "0";
+                    break;
+            }
+            return bllusp.Update(usp);
+        }
+        [WebMethod]
+        //用户权选择更改
+        public bool xuanzek(int userId, string type)
+        {
+            starweibo.Model.powerInfo usp = new powerInfo();
+            starweibo.BLL.powerInfo bllusp = new starweibo.BLL.powerInfo();
+            usp = bllusp.GetModel(userId);
+            switch (type)
+            {
+                case "zan":
+                    usp.zan = "1";
+                    break;
+                case "pinglun":
+                    usp.pinglun = "1";
+                    break;
+                case "at":
+                    usp.at = "1";
+                    break;
+                case "guanzhu":
+                    usp.guanzhu = "1";
+                    break;
+                case "jubao":
+                    usp.jubao = "1";
+                    break;
+                case "zhuanfa":
+                    usp.zhuanfa = "1";
+                    break;
+                case "shoucang":
+                    usp.shoucang = "1";
+                    break;
+                case "fasong":
+                    usp.fasong = "1";
+                    break;
+            }
+            return bllusp.Update(usp);
         }
     }
 }

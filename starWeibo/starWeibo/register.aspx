f0d770f477973f63079eb5611499dbcf848c4497
hytcshenyazhou <641077955@qq.com>
2014-10-21 14:14:06 +0800

    注册插入数据库
    实现插入数据库

--- starWeibo/starWeibo/register.aspx
+++ starWeibo/starWeibo/register.aspx
@@ -23,8 +23,11 @@
 						</li>
 						<li>
 								<div class="name1">安全邮箱：</div>
-								<input class="text_input youxiang" type="text"  name="uername"/>
+								<input class="text_input youxiang" type="text"  name="uername" a="1"/>
 								<div class="tishi ts2">请输入常用邮箱名</div>
+						</li>
+                        <li class="yincang">
+								<div class="yanzheng">请进行邮箱验证</div>
 						</li>
 						<li>
 								<div class="name1">密码：</div>
@@ -47,7 +50,7 @@
 							<span>我阅读并接受《星空微博服务协议条款》</span>
 					</div>
 					<div class="finishzhuce">
-						<a href="login.aspx">立即注册并登录</a>
+						<a>立即注册并登录</a>
 					</div>
 		</div>
 		<div class="register-know">
--- starWeibo/starWeibo/webservice/wsregister.asmx.cs
+++ starWeibo/starWeibo/webservice/wsregister.asmx.cs
@@ -13,7 +13,7 @@ namespace starWeibo.webservice
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。 
-    // [System.Web.Script.Services.ScriptService]
+    [System.Web.Script.Services.ScriptService]
     public class wsregister : System.Web.Services.WebService
     {
 
@@ -21,6 +21,17 @@ namespace starWeibo.webservice
         public string HelloWorld()
         {
             return "Hello World";
+        }
+        [WebMethod]
+        public string shenqing(string userName, string userPwd, string userMail)
+        {
+            starweibo.Model.userInfo shengqing = new starweibo.Model.userInfo();
+            starweibo.BLL.userInfo oneshenqing = new starweibo.BLL.userInfo();
+            shengqing.userName = userName;
+            shengqing.userPwd = userPwd;
+            shengqing.userMail = userMail;
+            return oneshenqing.Add(shengqing).ToString();
+
         }
     }
 }
